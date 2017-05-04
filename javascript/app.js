0866868fecf3e28ee5e37a54314a0ffb10a9bf1a
Jared Hanson <hansonj@mit.edu>
2017-05-04 16:53:23 -0400

    Removing old code from app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,58 +1,5 @@
 $(document).ready(function() {
 
-    $('.scroll-to-about').click(function() {
-        var nextSection = $('#about');
-        $('html, body').animate({
-            scrollTop: $(nextSection).offset().top
-        }, 2000);
-    });
-
-    $('.scroll-to-skills').click(function() {
-        var nextSection = $('#skills');
-        $('html, body').animate({
-            scrollTop: $(nextSection).offset().top
-        }, 2000);
-    });
-
-    $('.scroll-to-portfolio').click(function() {
-        var nextSection = $('#portfolio');
-        $('html, body').animate({
-            scrollTop: $(nextSection).offset().top
-        }, 2000);
-    });
-
-    $('.scroll-to-experience').click(function() {
-        var nextSection = $('#experience');
-        $('html, body').animate({
-            scrollTop: $(nextSection).offset().top
-        }, 2000);
-    });
-
-    $(".project").hover(function() {
-
-        $(this).css("border", "8px solid #ff3d2b");
-        $(this).css("border-width", "0 0 0 2px");
-
-    }, function() {
-        $(this).find(".project-logo").css("border", "2px solid rgba(245,245,245,1)");
-        $(this).css("border-style", "none");
-    });
-
-    function shakeDiv(divSelector) {
-
-        var l = 10;
-        for (var i = 0; i <= 10; i++) {
-            $(divSelector).animate({
-                'margin-left': '+=' + (l = -l) + 'px',
-                'margin-right': '-=' + l + 'px'
-            }, 30, function() {
-                $(divSelector).css('margin-left', '');
-                $(divSelector).css('margin-right', '');
-            });
-        }
-
-    }
-
     $(".answer-submit").click(function(event) {
         event.preventDefault();
         var selected = this.id;
@@ -120,8 +67,4 @@ $(document).ready(function() {
             window.location.href = "login.html";
         });
     });
-
-
-
-
 });
