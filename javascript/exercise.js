757c23aa2418496b9992b410718dce4e0bb6e4d6
Jared Hanson <hansonj@mit.edu>
2017-05-07 14:18:31 -0400

    fixes to templates for exercises

--- javascript/exercise.js
+++ javascript/exercise.js
@@ -18,14 +18,16 @@ $( document ).ready(function(){
 
     var exerciseId = getUrlParameter('id');
     console.log(exerciseId);
-    if ( !exerciseId ){location.href = '/main.html';}
+    if ( !exerciseId ){location.href = './main.html';}
 
     Request.GET('exercises/' + exerciseId.toString(), function(response){
         if (response && !response.error){
+
             var completedCount = response.problems.reduce(function(accum, curr){
                 if (curr.completed){ return accum + 1;}
                 else { return accum; }
-            }, 0)
+            }, 0);
+
             var titleHTML = Handlebars.templates['exercise_title']({
                 completedCount: completedCount,
                 exercise: response
@@ -43,6 +45,73 @@ $( document ).ready(function(){
 
             $('.game-container').html(exerciseGameContainerHTML);
 
+            $(".answer-submit").click(function(event) {
+                event.preventDefault();
+                var selected = this.id;
+                var num = selected[selected.length - 1];
+                var selectedId = "#" + num;
+
+                var answerId = "#answer-box" + num;
+                var answer = $(answerId).val();
+
+                var index = parseInt(num) - 1;
+                var correctAnswers = response.problems.map(function(curr){return curr.answer;});
+
+                if (parseInt(answer) == correctAnswers[index]) {
+                    const previouslyCompleted = parseInt($('text#completed-count')[0].innerHTML);
+                    const nowCompleted = previouslyCompleted + 1;
+                    $('text#completed-count')[0].innerHTML = nowCompleted;
+                    $(selectedId + " .image-snippet").css("visibility", "hidden");
+                    $(selectedId + " .curl").css("width", "240px");
+                    $(selectedId + " .curl").css("height", "240px");
+                    $(selectedId).animate({ opacity: 0 }, { duration: "slow" });
+
+
+                    //when finished with all of them
+                    if (nowCompleted == correctAnswers.length) {
+                        setTimeout(function() {
+                            $('.exercise-modal > .modal-contents').addClass('success');
+                            $('.exercise-modal').addClass('active');
+                            $('.exercise-modal > .modal-contents').empty();
+                            $('.exercise-modal > .modal-contents').append("<h1>Congrats, you're finished!</h1>");
+                            $('.exercise-modal > .modal-contents').append("<h3>What next?</h3>");
+                            $('.exercise-modal > .modal-contents').append("<div class='next'><a class='close-modal'>View Image</a><span>  |  <a href='./collections.html'>My Collection</a><span>  |  </span><a href='./exercise.html'>Next Exercise</a></div>");
+                            $('.next a.close-modal').on('click', function() {
+                                $('.exercise-modal > .modal-contents').removeClass('success');
+                                $('.exercise-modal').removeClass('active');
+                                $('.exercise-modal > .modal-contents').empty();
+                            });
+                        }, 1000);
+                    };
+                } else {
+                    var answerBoxId = "#answer-box" + num;
+                    var answerBox = $(answerBoxId)[0];
+                    answerBox.style.borderStyle = "none";
+                    answerBox.style.border = "1px solid #E52F4F";
+                    $(answerBoxId).effect("shake", { distance: 5, times: 3 }, 500)
+                    $(answerBoxId).focus();
+                    $("#check-button" + num)[0].style.visibility = "visible";
+                }
+
+
+
+            });
+
+            $(".answer-box").focus(function(event) {
+                var id = event.target.offsetParent.id;
+                console.log($("#check-button" + id))
+                $("#check-button" + id)[0].style.visibility = "visible";
+            });
+            $(".answer-box").blur(function(event) {
+                var id = event.target.offsetParent.id;
+                $("#check-button" + id)[0].style.visibility = "hidden";
+            });
+
+            $(".logout-button").click(function() {
+                Request.DELETE("signin", function() {
+                    window.location.href = "login.html";
+                });
+            });
 
         } else {
             if (response.status == 401 && response.error){
