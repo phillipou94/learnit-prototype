3f686a3d3aeea2143a77fec1fc20e8106b781f78
Phillip Ou <pou@mit.edu>
2017-05-09 14:21:21 -0400

    sorting working.

--- javascript/main.js
+++ javascript/main.js
@@ -7,34 +7,6 @@ $(document).ready(function() {
     $(".main-body").append(loader)
 
     Request.GET('home', function(response) {
-        response.exercises = [{
-                "id": 1,
-                "name": "Exercise 1",
-                "total_problems": 8,
-                "completed_problems": 8,
-                "due_date": "May 10, 2017"
-            }, {
-                "id": 2,
-                "name": "Exercise 1",
-                "total_problems": 8,
-                "completed_problems": 0,
-                "due_date": "May 11, 2017"
-            },
-            {
-                "id": 3,
-                "name": "Exercise 1",
-                "total_problems": 8,
-                "completed_problems": 1,
-                "due_date": "May 2, 2017"
-            },
-            {
-                "id": 4,
-                "name": "Exercise 4",
-                "total_problems": 8,
-                "completed_problems": 1,
-                "due_date": "May 12, 2017"
-            },
-        ]
 
         if (response && !response.error) {
             $(".loader-container").remove();
@@ -62,6 +34,43 @@ $(document).ready(function() {
             });
             $('.exercise-table').html(exerciseTabelHTML);
 
+            $(document).on('click', '.dropdown-option', function(event) {
+                var option = event.target.textContent;
+                var optionId = event.target.id;
+                var display = "Sort by " + option;
+
+                //sort by due date
+                if (optionId === "option1") {
+                    response.exercises.sort(function(a, b) {
+                        return new Date(a.due_date).getTime() > new Date(b.due_date).getTime();
+                    });
+                } else if (optionId === "option2") {
+                    //sort by incomplete first then by due date
+                    response.exercises.sort(function(a, b) {
+                        a.incomplete = a.completed_problems !== a.total_problems;
+                        b.incomplete = b.completed_problems !== b.total_problems
+                        return b.incomplete - a.incomplete ||
+                            new Date(a.due_date) - new Date(b.due_date);
+                    });
+
+                } else {
+                    //sort by completed first, then partially completed, then due date
+                    response.exercises.sort(function(a, b) {
+                        a.done = a.completed_problems === a.total_problems;
+                        b.done = b.completed_problems === b.total_problems
+                        return b.done - a.done ||
+                            b.completed_problems - a.completed_problems ||
+                            new Date(a.due_date) - new Date(b.due_date);
+                    });
+                }
+                var exerciseTabelHTML = Handlebars.templates['main_exercise_table']({
+                    exercises: response.exercises,
+                    progress: progressData || { percent: 0, value: 0 / 100.0 },
+                    sort_display: display
+                });
+                $('.exercise-table').html(exerciseTabelHTML);
+            });
+
         } else {
             $(".loader-container").remove();
             if (response.status == 401 && response.error) {
@@ -70,42 +79,6 @@ $(document).ready(function() {
             }
         }
 
-        $(document).on('click', '.dropdown-option', function(event) {
-            var option = event.target.textContent;
-            var optionId = event.target.id;
-            var display = "Sort by " + option;
-
-            //sort by due date
-            if (optionId === "option1") {
-                response.exercises.sort(function(a, b) {
-                    return new Date(a.due_date).getTime() > new Date(b.due_date).getTime();
-                });
-            } else if (optionId === "option2") {
-                //sort by incomplete first then by due date
-                response.exercises.sort(function(a, b) {
-                    a.incomplete = a.completed_problems !== a.total_problems;
-                    b.incomplete = b.completed_problems !== b.total_problems
-                    return b.incomplete - a.incomplete ||
-                        new Date(a.due_date) - new Date(b.due_date);
-                });
-
-            } else {
-                //sort by completed first, then partially completed, then due date
-                response.exercises.sort(function(a, b) {
-                    a.done = a.completed_problems === a.total_problems;
-                    b.done = b.completed_problems === b.total_problems
-                    return b.done - a.done ||
-                        b.completed_problems - a.completed_problems ||
-                        new Date(a.due_date) - new Date(b.due_date);
-                });
-            }
-            var exerciseTabelHTML = Handlebars.templates['main_exercise_table']({
-                exercises: response.exercises,
-                progress: progressData || { percent: 0, value: 0 / 100.0 },
-                sort_display: display
-            });
-            $('.exercise-table').html(exerciseTabelHTML);
-        });
     });
 
 
